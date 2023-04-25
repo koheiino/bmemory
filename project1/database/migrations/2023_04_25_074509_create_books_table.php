522b1eebbe9eba020e9a3b49bd2a35b8098357e2
東航平 <higashikohei@azumakouheinoMacBook-Air.local>
2023-04-25 17:39:50 +0900

    [Add]booksテーブルの設定

--- project1/database/migrations/2023_04_25_074509_create_books_table.php
+++ project1/database/migrations/2023_04_25_074509_create_books_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('bookname');
+            $table->text('impression'); 
             $table->timestamps();
         });
     }
